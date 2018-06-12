981a3534ef5416e8bdc3b9294c33fa50e453b5f0
zmy123111 <771250722@qq.com>
2018-06-12 18:54:16 +0800

    测试

---
+++ src/main/java/controller/IndexController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by ZMY on 2018/6/12.
+ */
+@Controller
+@RequestMapping("/home")
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String index() {
+        return "hello";
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ZMY
+  Date: 2018/6/12
+  Time: 16:54
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>hello spring</title>
+</head>
+<body>
+hello spring
+</body>
+</html>
