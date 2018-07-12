456de17a6162111012cad628601c0960cff84458
zmy123111 <771250722@qq.com>
2018-07-13 00:54:45 +0800

    222

--- src/main/java/controller/IndexController.java
+++ src/main/java/controller/IndexController.java
@@ -12,6 +12,7 @@ public class IndexController {
 
 @RequestMapping("/index")
     public String index() {
+    System.out.println("222");
     System.out.println("1111");
         return "hello";
     }
